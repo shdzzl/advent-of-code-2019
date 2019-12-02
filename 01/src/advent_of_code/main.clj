c092f774c33ef23c209d26f5a267ba7b139f0547
Shayden Martin <mail@shayden.me>
2019-12-03 00:43:55 +1100

    Day one solved

---
+++ 01/src/advent_of_code/main.clj
@@ -0,0 +1,16 @@
+(ns advent-of-code.main
+  (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]))
+
+(defn- total-fuel-required [modules]
+  (->> modules
+       (map #(/ % 3))
+       (map #(Math/floor %))
+       (map #(- % 2))
+       (reduce +)))
+
+(defn -main [& args]
+  (let [[input-filepath output-filepath] args
+        input-lines (line-seq (io/reader input-filepath))
+        modules (map edn/read-string input-lines)]
+    (spit output-filepath (int (total-fuel-required modules)))))
---
+++ README.md
@@ -0,0 +1,8 @@
+## Requirements
+
+- [Clojure command line tools](https://clojure.org/guides/getting_started)
+
+## Usage
+
+For all days, run `clj -m advent-of-code.main input output` to run the solution
+and write the result to `output`.
