438d4dc6aa8cdec08d708eeb20e6418a32252e8c
Shayden Martin <mail@shayden.me>
2019-12-03 01:21:35 +1100

    Day one part two solved

--- 01.2/src/advent_of_code/main.clj
+++ 01.2/src/advent_of_code/main.clj
@@ -2,15 +2,25 @@
   (:require [clojure.edn :as edn]
             [clojure.java.io :as io]))
 
+(def ^:private fuel-required
+  (comp #(- % 2)
+        #(Math/floor %)
+        #(/ % 3)))
+
 (defn- total-fuel-required [modules]
-  (->> modules
-       (map #(/ % 3))
-       (map #(Math/floor %))
-       (map #(- % 2))
-       (reduce +)))
+  (reduce + (for [module modules]
+              (loop [acc 0
+                     fuel (fuel-required module)]
+                (if (<= fuel 0)
+                  acc
+                  (recur (+ acc fuel) (fuel-required fuel)))))))
 
 (defn -main [& args]
   (let [[input-filepath output-filepath] args
         input-lines (line-seq (io/reader input-filepath))
         modules (map edn/read-string input-lines)]
     (spit output-filepath (int (total-fuel-required modules)))))
+
+(comment
+  (-main "input" "output")
+  )
